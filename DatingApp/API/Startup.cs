2c24fbea62ad76b839410b4f0d2653c54ed5736f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-09-26 00:43:16 +0300

    13. Creating the Connection string

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -18,12 +18,25 @@ namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        // 1. here we injecting our configuration into the startup class.
+        // 1. I don't really like it, so we'll follow a slightly different way of specifying dependency injection...
+        // 1. other developers are into this way too, it's not just me
+        private readonly IConfiguration _config;
+        // 4. When we construct this class, the IConfiguration is being injected into this class when it's constructed,
+        public Startup(IConfiguration config) //2. shorted the name and ctrl+. 'init field from parameter'
         {
-            Configuration = configuration;
+
+            /*3.*/
+            _config = config;
+            // Configuration = configuration;
+
+            //4. I don;t want to remove the 'this' and retype the variables so:
+            //4.1. for the _ retyping: in settings, add _ to 'Private Member Prefix'
+            //4.2. for removing 'this', uncheck 'Use This For Ctor Assignments'
+            //4.3. check the works by deleting line 30 and 24. follow point 2. again
         }
 
-        public IConfiguration Configuration { get; }
+        // public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -32,8 +45,9 @@ namespace API
             // don't use DbContext here 
 
 
-            services.AddDbContext<DataContext>(options => { // this is a lambda expression - very common if you want to pass expression as a parameter
-                options.UseSqlite("connection string"); // a dummy string,
+            services.AddDbContext<DataContext>(options =>
+            { // this is a lambda expression - very common if you want to pass expression as a parameter
+                options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
 
             services.AddControllers();
