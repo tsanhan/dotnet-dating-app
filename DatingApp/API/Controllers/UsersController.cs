65b95bf8762bfb24a4390666630b9d5746e1bc2d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-05 09:59:45 +0300

    16. Making our code Asynchronous

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -5,37 +5,42 @@ using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController] // 2.
-    [Route("api/[controller]")]// 3. convention over configuration
-    public class UsersController : ControllerBase // 1. first thing first
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
         private readonly DataContext _context;
-        public UsersController(DataContext context) // 4. DI the context and right click on context => initial field from parameter
+        public UsersController(DataContext context)
         {
             _context = context;
 
         }
 
-        // 5. adding endpoints for all the users and specific user:
         [HttpGet]
-        // 6. returning action result to wrap the data in a proper manner (we can dig into the class to see it support async operation)
-        // 7. the data is IEnumerable (a basic iteration capable list), we can use List but it's an overkill
-        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        // 1. this is sync code, this means that the tread is blocked until request fulfillment ( not scalable)
+        // 2. even through web servers are multi threaded, this is wastfull, (say we have 100K users... not practical)
+        // 3. how to make our code async: general rule: all DB access are async 
+        // 4. we want to pass the task to another thread and let me be fee to accept other requests
+        //  - this the passing is done using a Task (there is a Async vertion to ToList we'll see)
+        public /*5. making async*/ async /*6. returning a task (like a JS promise)*/Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
 
-            var users = _context.Users.ToList();
+            var users = /*7. await response (for unwrap result from task*/ await _context.Users.ToListAsync(); // 8. ToListAsync is an extention method to wrap ToList in a Task
+            // 9. another way to return the data and not a task but this is not a good because we still block until the task is finished
+            // var users = _context.Users.ToListAsync().Result;
+            // can read more here: https://stackoverflow.com/questions/13159080/how-does-taskint-become-an-int
             return users;
         }
 
-        // 8.
-        [HttpGet("{id}")] //id: route parameter:  api/users/3
-        public ActionResult<AppUser> GetUser(int id)
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
         {
 
-            return _context.Users.Find(id); // Find search by primary key
+            return await _context.Users.FindAsync(id); // Find search by primary key
         }
 
     }
