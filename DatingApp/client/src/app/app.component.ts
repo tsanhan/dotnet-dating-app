91b6f23a62336b5e670cefba7fe1c74e0293194f
Victor Yampolsky <tsanhan@gmail.com>
2021-10-10 13:14:52 +0300

    25. Displaying the fetched users in the browser

--- DatingApp/client/src/app/app.component.ts
+++ DatingApp/client/src/app/app.component.ts
@@ -8,35 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users: any // 3. ts gives up type safety unless we use 'any';
+  users: any
 
-  //1. we use DI using the constructor
-  constructor(private http: HttpClient/*can read from inline doc */) {
+  constructor(private http: HttpClient) {
 
   }
 
-  // 2. a life cycle event:
-  // after the ctor is done, angular invoke the oninit LS Hook 
-  // can read from OnInit inline doc
-  ngOnInit(): void /*3. return type from method (in this case - nothing) */ {
+  ngOnInit(): void {
     this.getUsers();
   }
 
   getUsers() {
-    //4. using http 
-    //on 'get' inline doc, show in parameters that '?' is optional, what the method returns
-    // it return Observable (not available in JS it's related to rxjs)
-    // they are streams of data, we wont go too deep here with Observables, 
     this.http.get('https://localhost:5001/api/users')
-      // they are lazy, they do nothing unless somebody subscribe
-      // all are optional
-      // way 1:
-      // .subscribe({
-      //   next: response => {}, // what to do with returned data 
-      //   error: error => {}, // what to do with error
-      //   complete: () => {} // what to do when complete
-      // })
-      // way 2:
-      .subscribe(response => { this.users = response }, error => { console.log(error); }, () => { }) // also all are optional
+
+      .subscribe(response => {
+        this.users = response
+      },
+        error => { console.log(error); }, () => { }) // also all are optional
   }
 }
