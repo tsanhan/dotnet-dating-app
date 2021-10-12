e2c3bb835a7ec76b7a82a4d09cfefb37059f2149
Victor Yampolsky <tsanhan@gmail.com>
2021-10-12 21:26:46 +0300

    33. Updating the user entity

---
+++ DatingApp/API/Data/Migrations/20211012181815_UserPasswordAdded.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            //1. we added two columns, 
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "BLOB", //2. what is BLOB? sqlite do't know what is byte array so the sqlite provider uses "Binary Large OBject" 
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+        }
+    }
+}
--- DatingApp/API/Entities/AppUser.cs
+++ DatingApp/API/Entities/AppUser.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 
 namespace API.Entities
 {
-    // the name AppUser is because User is a very used name
-    // so to distinguish between our apps user and a user coming from other part of .net 
     public class AppUser
     {
-        public int Id { get; set; } // EF use this name to make our lives easier (as for primary key, incrementaling etc..)
-        public string UserName { get; set; } // using name w/uppercase N because of conversion (will confuse us when well learn about .net core identity)
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        // these will be calculated and returned to un as byte arrays from our BE so we'll save them as such in our DB 
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
 
     }
 }
