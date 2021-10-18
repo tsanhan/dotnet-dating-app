dc6ec87313e411eb7cf78ea2c9302f7cac69586e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-18 09:31:42 +0300

    36. Using the debugger

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -19,29 +19,21 @@ namespace API.Controllers
 
         }
 
-        [HttpPost("register")]/*POST: http call to add resource, POST can contain Body*/
-        // build an action to register new users (that's why the POST)
-        /*
-            ApiConteroller maps the key to parameter, otherwise: [FromBody] string username... ,
-            because we can get data from different places (query string, route, header, etc...)
-         */
+        [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(string username, string password)
         {
-            // well use the hashing algo for pass hash
-            // 'using' when we finish using 'hmac' it will we disposed of (inherited from IDisposable (can investigate parents) - calling dispose() on destruction - like when exiting scope)
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
                 UserName = username,
-                PasswordHash = hmac.ComputeHash(/*need to provide bytes*/System.Text.Encoding.UTF8.GetBytes(password)),
-                PasswordSalt = hmac.Key // because it's randomly generated
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)),
+                PasswordSalt = hmac.Key
             };
 
-            _context.Users.Add(user); /*look at the description, here we only saying that we *want* to add, 
-                                        we do this by tracking this action and the entities related to this action*/
+            _context.Users.Add(user);
 
-            await _context.SaveChangesAsync(); // the actual operation
+            await _context.SaveChangesAsync();
             return user;
 
         }
