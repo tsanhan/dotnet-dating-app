603666d0349106be14f096e0f3338b8455cb2071
Victor Yampolsky <tsanhan@gmail.com>
2021-10-24 10:39:15 +0300

    43. Creating a User DTO and returning the token

--- DatingApp/API/Controllers/AccountController.cs
+++ DatingApp/API/Controllers/AccountController.cs
@@ -6,6 +6,7 @@ using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using API.Interfaces;
 
 namespace API.Controllers
 {
@@ -13,14 +14,17 @@ namespace API.Controllers
     public class AccountController : BaseApiController
     {
         private readonly DataContext _context;
-        public AccountController(DataContext context)
+        //1. inject the token service
+        private readonly ITokenService _tokenService;
+        public AccountController(DataContext context, ITokenService tokenService)
         {
+            _tokenService = tokenService;
             _context = context;
 
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+        public async Task<ActionResult<UserDto/*3. returning UserDto*/>> Register(RegisterDto registerDto)
         {
             if (await UserExist(registerDto.Username)) return BadRequest("Username is taken");
 
@@ -36,42 +40,40 @@ namespace API.Controllers
             _context.Users.Add(user);
 
             await _context.SaveChangesAsync();
-            return user;
+            //2. we want to return the token so we'll create a DTO for that
+            // create and go to DTOs/UserDto.cs
+
+            //4. return userDto
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
 
         }
 
-        //1. creating post endpoint (we send data in body)
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto) //2. we'll create a separate dto because later we'll ask for more from RegisterDto,create and go to LoginDto.cs 
+        public async Task<ActionResult<UserDto/*5. same here*/>> Login(LoginDto loginDto)
         {
-            //3. get user from db
-            /*in Users: 
-            checkout the method FindAsync method: it's nice if we getting by a PK, our username is not a PK 
-            option 2: FirstOrDefaultAsync, from inline doc: returns first element, this would work... 
-            option 3: SingleOrDefaultAsync: this one throe exception if >1 in the list after condition 
-            
-            ok so option 3: they are pretty much the same... */
 
             var user = await this._context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
             if (user == null) return Unauthorized("invalid username");
 
-            //4. check password... so using hmac todo the reverse of what we did to register:
-            // calculate the hash using the salt and the given password
-            
-            //there is an overload to this ctor.
-            // we want to hash to be created not with random key, 
-            //but with the salt (perviously generated key)
             using var hmac = new HMACSHA512(user.PasswordSalt);
 
             var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-            
-            //5. to compare with stored hash we need to loop over the bytearray:
+
             for (var i = 0; i < computedHash.Length; i++)
             {
-                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+                if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
             }
 
-            return user;
+             //6. return userDto
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
 
         }
 
