69fa230386f53f4bcb92c24bd053cb8816c237c5
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-10-25 11:20:01 +0300

    49. Introduction to Angular template forms

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-
+  //1. using 2 way binding (updating the view <==> component)
+  model: any = {};
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  //2. on login lets see what the model holds
+  //go to the view
+  login() {
+    console.log(this.model);
+
   }
 
 }
