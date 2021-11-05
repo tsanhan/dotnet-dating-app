025c30493fc83e0cb87e0ec3f94578e35343032d
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-05 15:36:19 +0200

    58. Adding a register form

---
+++ DatingApp/client/src/app/register/register.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  // add all this and go to the template
+  model: any = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register() {
+    console.log(this.model);
+  }
+
+  cancel() {
+    console.log('cancelled');
+  }
+
+
+
+}
