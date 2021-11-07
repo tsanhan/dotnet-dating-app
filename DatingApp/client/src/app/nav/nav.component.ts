5d81a0a711f42eff6623a52073b2ba9592dad008
Victor Yampolsky <tsanhan@gmail.com>
2021-11-07 10:12:08 +0200

    66. Routing in code

--- DatingApp/client/src/app/nav/nav.component.ts
+++ DatingApp/client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from '../models/user';
 import { AccountService } from '../services/account.service';
@@ -9,36 +10,24 @@ import { AccountService } from '../services/account.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  //2. remove the loggedIn property from the nav component
-  // loggedIn: boolean = false;
-
-  //3. use currentUser$ to get the current user
   currentUser$: Observable<User>;
-
-
   model: any = {};
-  constructor(private accountService: AccountService) {
+
+  constructor(private accountService: AccountService,
+    /*1. inject the router*/ private router :Router) {
+
     this.currentUser$ = this.accountService.currentUser$;
   }
 
   ngOnInit(): void {
-    //7. we dont need this anymore
-    // this.getCurrentUser();
-
-    //8. go to nav.component.html and switch the ngIf to the currentUser$
-    //9. two things cna be changed here:
-    // 1 we can use this.accountService.currentUser$ in the template, no need for this.currentUser$
-    // 2 to do 1 we need the accountService in the constructor to be public
-
-    //10. we can remove the logout() link in the nav.component.html
   }
 
   login() {
     this.accountService.login(this.model)
       .subscribe(response => {
+        //2. navigate
+        this.router.navigateByUrl('/members');
         console.log(response);
-        // 4. no need of this anymore
-        // this.loggedIn = true;
       },
       error => {
         console.log(error);
@@ -48,22 +37,8 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    // 5. no need of this anymore
-    // this.loggedIn = false;
+    //3. navigate
+    this.router.navigateByUrl('/');
   }
-
-  //6. we dont need this anymore
-  // getCurrentUser() {
-  //   //1. we should not use subscribe here:
-  //   // this current user is not an http request, this never completes: potential for memory leak (explain why - what async pipe do?)
-  //   // lets use the async pipe
-  //   this.accountService.currentUser$.subscribe(user => {
-  //     this.loggedIn = !!user;
-  //   },
-  //   error => {
-  //     console.log(error);
-
-  //   })
-  // }
 
 }
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-adding the nav links above
+using the router to navigate:
+now: when in `/members` say, on logout I still there, I want to navigate out,
+and, when they log in, I want to navigate to `/members`
 
-go to nav.component.html
+go to nav.component.ts
 
-up next: access the router inside our component
+up next: adding toast to notify the user if something gone wrong (or right)
