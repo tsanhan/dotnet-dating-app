1e1e8228a356684d6ee8f423d9f4a9a266e1701f
Victor Yampolsky <tsanhan@gmail.com>
2021-11-07 09:48:24 +0200

    64. Creating some more components

--- DatingApp/client/src/app/app-routing.module.ts
+++ DatingApp/client/src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
+import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [ // we'll use this array to provide routes for angular
+  { // will be loaded on http://localhost:4200/ without any routing
+    path:'',
+    component: HomeComponent,
+    pathMatch: 'full' // better read the inline doc on pathMatch (hover above it)
+  }, {
+    path:'members',
+    component: MemberListComponent
+  },
+  {
+    path:'members/:id', //:id is a parameter, for example 4 will look like: http://localhost:4200/members/4
+    component: MemberDetailComponent
+  },
+  {
+    path:'lists',
+    component: ListsComponent
+  },
+  {
+    path:'messages',
+    component: MessagesComponent
+  },
+  {
+    path:'**', //wildcard route (on error/non existing route)
+    component: HomeComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
