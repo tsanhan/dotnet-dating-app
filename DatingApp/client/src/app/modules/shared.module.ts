553546b8a96f0c1fa6f2191ddfb83a8897362a17
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-10 08:43:05 +0200

    72. Tidying up the app module by using a shared module

---
+++ DatingApp/client/src/app/modules/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ToastrModule } from 'ngx-toastr';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    // taken from app.module.ts, remove it from there and remove imports
+    BsDropdownModule.forRoot(),
+    ToastrModule.forRoot({
+      positionClass:'toast-bottom-right'
+    }),
+  ],
+  exports: [
+    BsDropdownModule,
+    ToastrModule
+  ]
+})
+export class SharedModule { }
