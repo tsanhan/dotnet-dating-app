f5808329259152a3416af2c89613796d250d8164
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-10 09:10:12 +0200

    72. Tidying up the app module by using a shared module

--- DatingApp/client/src/app/app-routing.module.ts
+++ DatingApp/client/src/app/app-routing.module.ts
@@ -3,8 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 
 const routes: Routes = [
@@ -19,8 +17,10 @@ const routes: Routes = [
     runGuardsAndResolvers:'always',
     canActivate:[AuthGuard],
     children:[
-      {path:'members',component: MemberListComponent},
-      {path:'members/:id',component: MemberDetailComponent},
+      {
+        path: 'members',
+        loadChildren: () => import('./modules/members.module').then(m => m.MembersModule)
+      },
       {path:'lists',component: ListsComponent},
       {path:'messages', component: MessagesComponent}
     ]
