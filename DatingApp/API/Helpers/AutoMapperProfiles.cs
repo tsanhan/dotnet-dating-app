7bf01cc0196455a44cc3c02db9b40af22f8cac1f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-19 15:01:43 +0200

    97. Configuring AutoMapper

--- DatingApp/API/Helpers/AutoMapperProfiles.cs
+++ DatingApp/API/Helpers/AutoMapperProfiles.cs
@@ -1,17 +1,24 @@
+using System.Linq;
 using API.DTOs;
 using API.Entities;
 using AutoMapper;
 
 namespace API.Helpers
 {
-    public class AutoMapperProfiles: Profile // we derive from Profile to get some cool features offed
+    public class AutoMapperProfiles : Profile // we derive from Profile to get some cool features offed
     {
         public AutoMapperProfiles()
-        {   // we want to map AppUser => MemberDto
-            CreateMap<AppUser, MemberDto>();
-            
-            // we want to map Photo => PhotoDto
+        {
+            CreateMap<AppUser, MemberDto>()
+            //1. we can configure a single property mapping
+            .ForMember(
+                dest => dest.PhotoUrl, // 1. the first part: what to map
+                opt => // 2. the second part is the options
+                    {  // 3. we specify the options to have the custom mapping (how to map the photoUrl)
+                        opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                    });
             CreateMap<Photo, PhotoDto>();
+
         }
     }
 }
