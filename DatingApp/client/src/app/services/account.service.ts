b8542c6aee3ad74d1dc75a6d9a598cf435d39be5
Victor Yampolsky <tsanhan@gmail.com>
2021-11-21 13:00:19 +0200

    103. Adding a member service

--- DatingApp/client/src/app/services/account.service.ts
+++ DatingApp/client/src/app/services/account.service.ts
@@ -2,14 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ReplaySubject } from 'rxjs';
 import { map } from "rxjs/operators";
+import { environment } from 'src/environments/environment';
 import { User } from '../models/user';
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-
-  baseUrl = 'https://localhost:5001/api/';
-  private currentUserSource$ = new ReplaySubject<User>(1);// 3. don't worry if u not clear on what is going on here... we'll come back to this when we look at routing
+  //1. lets remove this and use the environment files
+  //2. go to environment.ts
+  //3. go to environment.prod.ts
+  //4. name sure environment is imported from environment and not environment.prod
+  baseUrl = environment.apiUrl;
+  private currentUserSource$ = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource$.asObservable();
 
   constructor(private http: HttpClient) { }
@@ -27,7 +31,7 @@ export class AccountService {
   }
 
 
-  //1. create the resister method
+
   register(model: any) {
     return this.http.post<User>(this.baseUrl + 'account/register', model)
     .pipe(
@@ -36,11 +40,11 @@ export class AccountService {
           localStorage.setItem('user', JSON.stringify(user));
           this.currentUserSource$.next(user);
         }
-        return user; // first try without this line(this line is't needed): we'll console 'undefined' in the console in register.component.ts
+        return user;
       })
     )
   }
-  //2. go to register component
+
 
   setCurrentUser(user: User) {
     this.currentUserSource$.next(user);
---
+++ DatingApp/client/src/app/services/members.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Member } from '../models/member';
+
+// 2. remember we have users protected by authentication, we need to pass the token in the headers
+// this is a short term option
+const htpOptions = {
+  headers: new HttpHeaders({
+    Authentication: 'Bearer ' + JSON.parse(localStorage.getItem('user') as string).token
+  })
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+  baseUrl = environment.apiUrl;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  //1. start implementing
+  getMembers() {
+    return this.http.get<Member[]>(`${this.baseUrl}users'`,/*3. */ htpOptions);
+  }
+
+  //4.
+  getMember(username: string) {
+    return this.http.get<Member>(`${this.baseUrl}users/${username}`, htpOptions);
+  }
+}
