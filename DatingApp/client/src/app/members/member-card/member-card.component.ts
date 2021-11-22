24f509cc7933e10cf953109b75889120427f4df1
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-22 09:34:18 +0200

    106. Adding some style to the cards

--- DatingApp/client/src/app/members/member-card/member-card.component.ts
+++ DatingApp/client/src/app/members/member-card/member-card.component.ts
@@ -1,15 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { Member } from 'src/app/models/member';
 
 @Component({
   selector: 'app-member-card',
   templateUrl: './member-card.component.html',
-  styleUrls: ['./member-card.component.css']
+  styleUrls: ['./member-card.component.css'],
+  // 1. options here (can view in inline doc)
+  // a. encapsulation: ViewEncapsulation.ShadowDom: (same result as angular but natively in the browser) need to explicitly specify in the code we use (https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM)
+  // b. encapsulation: ViewEncapsulation.Emulated: default.
+  // c. encapsulation: ViewEncapsulation.None: no encapsulation.
 })
 export class MemberCardComponent implements OnInit {
-  // 1. get the member as input
   @Input() member!: Member;
-  // 2. go to the template
   constructor() { }
 
   ngOnInit(): void {
