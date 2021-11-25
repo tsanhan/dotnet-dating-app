c1ee6a98dd56644debfbddbe510889d02f4eb9ab
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-25 16:06:12 +0200

    117. Adding the update functionality

--- DatingApp/client/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp/client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
 import { Member } from 'src/app/models/member';
 import { User } from 'src/app/models/user';
@@ -11,29 +13,47 @@ import { MembersService } from 'src/app/services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
-  // 1. declare the member and the current user
+  //4. after point 8. in the template:
+  // creating a reference to the form in the template
+  @ViewChild('editForm') editForm: NgForm;
+
   member: Member;
   user:User;
 
   constructor(
-    //2. to populate the member and the current user I need the services
     private accountService: AccountService,
     private memberService: MembersService,
-
+    private toastr: ToastrService//2. add the toastr service
     ) {
-      //3. at first I need the user data
-      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user); // you can say it's synchronous
+      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
     }
 
   ngOnInit(): void {
-    //5. I need to get the member data
     this.loadMember();
-    //6. go to the template to show the member username
   }
 
-  //4. I need to get the member data based on the username
   loadMember() {
     this.memberService.getMember(this.user.username).subscribe(member => this.member = member);
+  }
+
+  //1. update the member
+  updateMember() {
+    // place holder for update member
+    console.log(this.member);
+
+    //3. use the service and go back to the template
+    this.toastr.success('Profile updated successfully');
+
+    //5. reset the form with the member value:
+    this.editForm.reset(this.member);
+    //6. test in the browser, after submitting, the alert disappears and the button is disabled
+    //7. the reset functionality in the NgForm
+    //   * the functionality is using the 'name' attribute in the template to bind to the form controls
+    //   * the ngModel is to bind to the member property
+    //   * we can the difference when if we change the 'name' (say from lookingFor to lookingfor)
+    //   * after the submit (editForm.reset run) we see nothing in lookingFor
+    //   * this is because NgForm could not find a form control for 'lookingfor'
+    //   * go to the template and change the 'name' attribute to test this
   }
 
 }
