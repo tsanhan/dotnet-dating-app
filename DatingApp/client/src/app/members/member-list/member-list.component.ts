b95e40ff4b64d49ff2f57fd3a88d0ea79e4afb0f
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-11-26 14:34:49 +0200

    122. Using the service to store state

--- DatingApp/client/src/app/members/member-list/member-list.component.ts
+++ DatingApp/client/src/app/members/member-list/member-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Member } from 'src/app/models/member';
 import { MembersService } from 'src/app/services/members.service';
 
@@ -8,26 +9,26 @@ import { MembersService } from 'src/app/services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  // 1. store members
-  members: Member[] = [];
+  //1. first we'll say our members will be an observable
+  members$: Observable<Member[]>
 
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
-    //3. call member
-    this.leadMembers();
+    //2. populate the members Observable
+    this.members$ = this.memberService.getMembers();
   }
 
-  //2. implement a method to fetch data
-  leadMembers(){
-    this.memberService.getMembers()
-    .subscribe(members => {
-      this.members = members; // no need to worry about errors 😉
-    })
-  }
+  //3. we don't need the leadMembers method anymore
+  // leadMembers(){
+  //   this.memberService.getMembers()
+  //   .subscribe(members => {
+  //     this.members = members;
+  //   })
+  // }
 
-  //4. test the component using the browser (login and enter localhost:4200/members)
-
-  // 5. fix errors from last commit in MembersService if not working [can give as task to class] (1. Authentication => Authorization, 2.  as any)?.token, 3. users'` => users`)
-
+  //4. so how will get the data from the observable?
+  // * we'll use the async pipe,
+  // * async pipe subscribes and unsubscribes when the component is destroyed
+  // * go to the html and add the async pipe
 }
--- DatingApp/client/src/app/services/members.service.ts
+++ DatingApp/client/src/app/services/members.service.ts
@@ -1,33 +1,62 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { Member } from '../models/member';
 
 
 
-
+//1. remember: this service is a singleton
+// * question when is it created? (on first inject, a component need the service)
+// * when is it destroyed? (on app close)
+// * so services are good fit to store application state
+//    * there are other things like redux, mobx, etc... all these be an overkill for our small app
+// * our service is good enough to store application state
+// * if we loaded the members before, we don't need to load them again
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+
+  //2. our members state;
+  members:Member[] = [];
+
   constructor(
     private http: HttpClient
   ) { }
 
+
+  //3. edit this method to check if we already have members stored
   getMembers() {
-    return this.http.get<Member[]>(`${this.baseUrl}users`);
+    if(this.members.length > 0) {
+      return of(this.members); //'of' will create an observable wil one value
+    }
+    return this.http.get<Member[]>(`${this.baseUrl}users`).pipe(
+      // tap is a operator that will do something with the result (not changing the result itself)
+      tap(members => this.members = members)
+    );
   }
 
-
+  //4. edit this method to check if we already have members stored
   getMember(username: string) {
+    const member = this.members.find(m => m.username === username);
+    if(member !== undefined) {// if we found the member
+      return of(member);
+    }
     return this.http.get<Member>(`${this.baseUrl}users/${username}`);
   }
 
-  //.1 add this
+  //5. edit this method to check if we already have members stored to update them too!
   updateMember(member: Member) {
-    return this.http.put(`${this.baseUrl}users`, member);
+    return this.http.put(`${this.baseUrl}users`, member).pipe(
+      tap(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
+  //6. go to the member-list.component.ts to do something different with the members
 
-  // 2. go to member-edit.component.ts
 }
