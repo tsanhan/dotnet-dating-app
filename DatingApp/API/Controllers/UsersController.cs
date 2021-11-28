fe9d210ee8a64fd586cea84532059ef8bdf08064
Victor Yampolsky <tsanhan@gmail.com>
2021-11-28 13:54:22 +0200

    130. Using the Created At Route method

--- DatingApp/API/Controllers/UsersController.cs
+++ DatingApp/API/Controllers/UsersController.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
         private readonly IMapper _mapper;
         private readonly IPhotoService _photoService;
         private readonly IUserRepository _userRepository;
-        public UsersController(IUserRepository userRepository, IMapper mapper, /*1. inject photo service*/ IPhotoService photoService)
+        public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
         {
             _mapper = mapper;
             _photoService = photoService;
@@ -38,7 +38,7 @@ namespace API.Controllers
             return Ok(users);
         }
 
-        [HttpGet("{username}")]
+        [HttpGet("{username}", /*3. add this */Name = "GetUser"),]
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             var rtn = await _userRepository.GetMemberAsync(username);
@@ -49,7 +49,6 @@ namespace API.Controllers
         [HttpPut]
         public async Task<ActionResult> UpdateUser(MemberUpdateDTO memberUpdateDTO)
         {
-            //4. use the extention
             var username = User.GetUsernae();
             var user = await _userRepository.GetUserByUserNameAsync(username);
 
@@ -66,15 +65,13 @@ namespace API.Controllers
 
         }
 
-        //2. HTTP: we creating a new resource
         [HttpPost("add-photo")]
         public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
         {
-            // 1. make sure we have somthing in the file property (the file name right)
             var username = User.GetUsernae();
             var user = await _userRepository.GetUserByUserNameAsync(username);
 
-            var result = await _photoService.UploadPhotoAsync(file);// 'step into' the UploadPhotoAsync
+            var result = await _photoService.UploadPhotoAsync(file);
 
             if(result.Error != null){
                 return BadRequest(result.Error.Message);
@@ -91,8 +88,25 @@ namespace API.Controllers
 
             user.Photos.Add(photo);
 
-            if(await _userRepository.SaveAllAsync())
-                return _mapper.Map<PhotoDto>(photo);
+            if(await _userRepository.SaveAllAsync()){
+                
+                //0. lets talk a bit about overloads:
+                //1. commenting the current return: return _mapper.Map<PhotoDto>(photo);
+                //2. we can return Created object but we'll need to somehow get the uri to create the object that contain the data. 
+                // * better use CreatedAtRoute so we can utilize existing routes (we have a route that returning the user that contains the photo)
+                // * for that we need to name a route that contains can give me the object I want
+                // * add name to the GetUser method
+
+                //4. use the name of the route to generate 201 status that also tell the client how to get the resource containing the object returned
+                // 4.1 return CreatedAtRoute("GetUser", _mapper.Map<PhotoDto>(photo));
+                
+                //5. test with postman: add photo to see the response, [this will fail]
+                //5.1 try to think a bit how would to fix this?
+
+                //6. the solution is o use another overload:
+                return CreatedAtRoute("GetUser",new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+                //7. test again, yay, success!,  on success checkout the location header created
+            }
             
             return BadRequest("Problem adding photos");
         }
