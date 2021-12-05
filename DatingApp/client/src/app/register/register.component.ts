2bec7320274df154ead0bae4bee90c7b05637a2c
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-05 08:30:33 +0200

    146. Using the form builder service

--- DatingApp/client/src/app/register/register.component.ts
+++ DatingApp/client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, Validator, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validator, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
@@ -17,48 +17,37 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private accountService: AccountService,
-    private toastr: ToastrService) { }
+    private toastr: ToastrService,
+    private fb: FormBuilder//1. inject the service
+    ) { }
 
   ngOnInit(): void {
     this.initializeForm();
   }
   initializeForm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl("Hello", Validators.required),
-      password: new FormControl('', [
+    //2. change this to use the form builder
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [
         Validators.required,
         Validators.minLength(4),
         Validators.maxLength(8),
-       ]),
-      confirmPassword: new FormControl('', [
+       ]],
+      confirmPassword:['', [
         Validators.required,
-         /*2. use the validator*/this.matchValues('password')
-      ]),
-      //3. testing the functionality in the browser
-      // * this looks like it's working but it's not:
-      // * AFTER getting a valid state, try to change the password field
-      // * why do you think it's not working?
-      // * answer: because the validator is running only on confirmPassword change
-      // * how would you fix this? (remember we can listen to control changes and we can check validity)
-      // * we can just add the same validator to the password field (is it a good idea?)
-      //   * try it' tell me what happens
-      //   * the problem here is that on the first right match the other field is still not valid
-      // * we can listen to changes in the password field and update the validity of the confirmPassword field
+         this.matchValues('password')
+      ]],
+
     });
-    //4.
     this.registerForm.get('password')?.valueChanges.subscribe(() => {
       this.registerForm.get('confirmPassword')?.updateValueAndValidity();
     });
   }
 
-  //1. add a validator (a function returning a ValidatorFn)
   matchValues(matchTo: string): ValidatorFn {
-    // all our consols are derived from AbstractControl (can go up the parent chain to see)
     return (control: AbstractControl): { [key: string]: any } | null => {
 
-      // we checking this ⬇️ value against the value of the matchTo control
       return control?.value === (control?.parent as FormGroup)?.controls[matchTo].value ? null : { isMatching: true };
-      // if the control is not valid, return {isMatching: true} (the validator error)
     }
 
   }
--- README.md
+++ README.md
@@ -1,11 +1,4 @@
-Creating a reusable text input
+Using the form builder service:
+go to register.component.ts
 
-create a folder called 'forms' and create a complete named 'text-input'.
-this will be a reusable component that can be used in multiple places.
-what we want to do there is to implement a 'control value accessor'.
-we want to access the FormControl object in text-input.
-
-go to text-input.component.ts.
-
-ok so we reduced the code for creating form controls to one line,
-up next: using the form builder service to make our code a little cleaner.
+up next: expend our form to have more information about the user on register
