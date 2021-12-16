08559f1d27cbd9c925e4cc41d00d8706aa83bc88
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-17 00:41:12 +0200

    174. Implementing the likes repository

--- DatingApp/API/Data/LikesRepository.cs
+++ DatingApp/API/Data/LikesRepository.cs
@@ -1,34 +1,73 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    //1. implement the interface
     public class LikesRepository : ILikesRepository
     {
-        //2. inject the context
         private readonly DataContext _context;
         public LikesRepository(DataContext context)
         {
             _context = context;
         }
-        //3. add this repository as a service in our app, go to ApplicationServiceExtensions.cs
-        public Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
+        
+        public async Task<UserLike> GetUserLike(int sourceUserId, int likedUserId)
         {
-            throw new System.NotImplementedException();
+            //1. start with the easyest one, just get the user like by the primary key
+            return await _context.Likes.FindAsync(sourceUserId, likedUserId);
         }
 
-        public Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+        public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
         {
-            throw new System.NotImplementedException();
+            //3. this is a bit tricky one, we'll query baked on predicate
+            //   * predicate: 'liked' or 'liked by', so the question are:
+            //   * 1. 'liked' => "which users this user has liked" => userId is the source user 
+            //   * 2. 'liked by' => "which users have been liked by this user" => userId is the liked user
+
+           
+            IQueryable<AppUser> users;
+            var likes = _context.Likes.AsQueryable();//get the likes
+            //4. looks like 2 queries, but not really, 
+            //  * we'll be joining the two and let EF figure out the join query for the db 
+
+            //5. search for which users this user has liked
+            if(predicate == "liked") {
+                likes = likes.Where(like => like.SourceUserId == userId); //filter
+                users = likes.Select(like => like.LikedUser); // select the relevant users (overriding initial users select)
+            }
+            //6. search for users that have been liked by this user
+            else {//if predicate == "likedBy"
+                likes = likes.Where(like => like.LikedUserId == userId); //filter
+                users = likes.Select(like => like.SourceUser); // select the relevant users (overriding initial users select)
+            }
+            
+            //7. we''ll use the DTO to select the properties we intersted in,
+            //   * no need to configure Mapping here, these are not so mulch properties
+            return await users.Select(user => new LikeDto
+            {
+                Username = user.UserName,
+                KnownAs = user.KnownAs,
+                Age = user.DateOfBirth.CalculateAge(),
+                PhotoUrl = user.Photos.FirstOrDefault(p => p.IsMain).Url,
+                City = user.City,
+                Id = user.Id
+            }).ToListAsync();
+            //8. back to readme.md
+
         }
 
-        public Task<AppUser> GetUserWithLikes(int userId)
+        public async Task<AppUser> GetUserWithLikes(int userId)
         {
-            throw new System.NotImplementedException();
+            //2. also simple: get the users with his likes included
+            return await _context.Users
+                .Include(u => u.LikedUsers)// when a user will ad a like it will be added to the likedUsers list here
+                .FirstOrDefaultAsync(u => u.Id == userId);
         }
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-Adding a likes repository:
-so we'll create a new repository called "LikesRepository"
-for that we'll start with the interface, create and go to Interfaces/ILikesRepository.cs
+Implementing the likes repository:
+go to LikesRepository.cs
 
-after we created the added the repository to our DI as a service we'll implement the repo 
-up next: implementing the likes repository
+now that er implemented the repository, we'll create and use it in a controller
+
+up next: create a controller for the likes.
