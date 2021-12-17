5660d73dc932c90e4a7d652ec690fdf508b708f7
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2021-12-17 09:41:52 +0200

    175. Controller methods for the likes feature

---
+++ DatingApp/API/Controllers/LikesController.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    //1. derive from BaseApiController nad make sure we authenticate the user
+    [Authorize]
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILikesRepository _likesRepository;
+
+        //2. inject repositories for users and likes
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            _likesRepository = likesRepository;
+            this._userRepository = userRepository;
+        }
+
+        //3. a mothod to like another user.
+        //  * we create an entity but we dore return it? why?
+        //  * we return data from POST so the client get the data created, here the client knows everything about the created entity
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username)
+        {
+            //4. get the current user id
+            var sourceUserId = User.GetUserId();
+
+            //5. get the user to like
+            var likedUser = await _userRepository.GetUserByUserNameAsync(username);
+
+            //6. get the source user
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            //7. let's add some checks:
+            //  * did't find the user to like?
+            if (likedUser == null) return NotFound();
+            
+            //  * does the user want to like themself?
+            if (sourceUser.UserName == username) return BadRequest("you can't like yourself");// a bit grim this one... 
+
+            //  * does the user already like the user to like?
+            var userLike = await _likesRepository.GetUserLike(sourceUserId, likedUser.Id);
+            if (userLike != null) return BadRequest("you already like this user");
+
+            //8. create the like
+            userLike = new UserLike
+            {
+                SourceUserId = sourceUserId,
+                LikedUserId = likedUser.Id
+            };
+
+            sourceUser.LikedUsers.Add(userLike);
+
+            //9. save the changes to the database
+            // * we don't have save method in likes repository so we use the user repository
+            // * this is temporary, now that that we have more then one repository...
+            // * we need to think about how many instances of DataContext we have
+            // * we'll think about a better option later 
+            // * specifically, how to handle multiple saving of data, and all the entities EF is tracking
+            // * and the architecture pattern for that (it's called 'unit of work')
+            if (await _userRepository.SaveAllAsync()) return Ok();
+            
+            return BadRequest("Failed to like user");
+            
+
+            
+        }
+    
+        //10. a method to get the users (as LikeDto) related to a user
+        public async Task<ActionResult<IEnumerable<LikeDto>>> GetUserLikes(string predicate)
+        {
+            var users = await _likesRepository.GetUserLikes(predicate, User.GetUserId());
+            return Ok(users);
+        }
+
+        //11. back to readme.md
+       
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,14 @@
-Implementing the likes repository:
-go to LikesRepository.cs
+Controller methods for the likes feature:
+ok so we'll create a controller for our like:
+create ang do to Controllers/LikesController.cs
 
-now that er implemented the repository, we'll create and use it in a controller
+after we have the controller test it, section 14:
+* first we login
+* then we create a like, success! if you get an 404 try resetting the API
+* then 'get liked users' (`{{url}}/api/likes?predicate=liked`), success, got the data
+* then 'Get Liked Users Liked by' (`{{url}}/api/likes?predicate=likedBy`), success, no data
+* you can test the other way around by logging in as the user you liked, and run 'Get Liked Users Liked by', success!, got the data about the first user
 
-up next: create a controller for the likes.
+up next: setting this in the client
+
+
