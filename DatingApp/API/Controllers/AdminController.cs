301f0a590d03cbe0f2f890fcbe1e285c9a4db71b
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-01-06 11:45:27 +0200

    210. Getting the users with roles

--- DatingApp/API/Controllers/AdminController.cs
+++ DatingApp/API/Controllers/AdminController.cs
@@ -2,33 +2,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Entities;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
     public class AdminController : BaseApiController
     {
-        //we'll have only 2 endpoints:
+        private readonly UserManager<AppUser> userManager;
 
-        //1. get all users - testing the authorization
-        [Authorize(Policy = "RequireAdminRole")] //we don't have this policy yet, so we'll create it later
-        [HttpGet("users-with-roles")]
-        public  ActionResult GetUsersWithRoles()
+        //1. add a constructor to inject what we need
+        public AdminController(UserManager<AppUser> userManager)
         {
-            return Ok("Only admins can see this");
+            this.userManager = userManager;
         }
 
 
-        //2. get photos to moderate - testing the authorization
-        [Authorize(Policy = "ModeratePhotoRole")] //we don't have this policy yet, so we'll create it later
+        [Authorize(Policy = "RequireAdminRole")] 
+        [HttpGet("users-with-roles")]
+        //2. make the method async that return a task 
+        public async Task<ActionResult> GetUsersWithRoles()
+        {
+            //3. get all users
+            var users = await userManager.Users
+            .Include(r => r.UserRoles)  // go to the joint table
+            .ThenInclude(r => r.Role)   // to get to the roles table
+            .OrderBy(r => r.UserName)   // order by username
+            .Select(u => new            // and project to anonymous object
+            {
+                u.Id, // id  
+                Username = u.UserName, // username
+                Roles = u.UserRoles.Select(r => r.Role.Name).ToList() // list of roles the user in 
+            })
+            .ToListAsync();
+
+            // we return the results, not paginated (we allready learned that), you can paginate, as HW 
+            return Ok(users);
+            //4. back to README.md
+        }
+
+
+        [Authorize(Policy = "ModeratePhotoRole")] 
         [HttpGet("photos-to-moderate")]
         public  ActionResult GetPhotosForModeration()
         {
             return Ok("Admins or moderators can see this");
         }
 
-        //3. now we need to create these policies
-        // * go to IdentityServiceExtensions.cs
+    
     }
 }
--- README.md
+++ README.md
@@ -1,18 +1,10 @@
-Adding policy based authorization:
-policy is kind of a group of rules, like "if a user has a role, and that role has permission (part of the policy), then he can do something"
+Getting the users with roles:
+- the odd thing is that the user manager don't have this functionality.
+- we'll have to get it ourselves by querying the database.
+- th reason is that we want admin to be able to update the users roles.
+- go to AdminController.cs
 
-we'll start implementing the policy based authorization with a controller only for the admin.
-create and go to AdminController.cs
+test the users-with-roles endpoint:
+Section 16: 'Get Roles as admin' success!
 
-lets test the roles via the admin controller:
-* testing the 'RequireAdminRole' policy:
-    * Section 16: 'Get Roles as admin', (using the admin token) success!
-    * Section 16: 'Get Roles as [login]' (using a member token) 403 Forbidden (cool!)
-
-* testing the 'ModeratePhotoRole' policy:
-    * Section 16: 'Get Photos to moderate as admin' (using the admin token) success!
-    * Section 16: 'Get Photos to moderate as [login]' (using a member token) 403 Forbidden (cool!)
-
-great, our policies are working!
-
-up next: implement logic for the 'users-with-roles' endpoint, returning the users with their roles 
+up next: edit the roles of the users.
