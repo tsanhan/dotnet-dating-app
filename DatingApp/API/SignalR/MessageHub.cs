6e26bb5eb0d5e91c828b25e61ba11808f0e5bd7e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-01-21 15:25:30 +0200

    232. Updating the message hub with group tracking

--- DatingApp/API/SignalR/MessageHub.cs
+++ DatingApp/API/SignalR/MessageHub.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System;
 using System.Threading.Tasks;
 using API.DTOs;
@@ -18,7 +19,7 @@ namespace API.SignalR
         public MessageHub(
             IMessageRepository messageRepository,
             IMapper mapper,
-            IUserRepository userRepository 
+            IUserRepository userRepository
             )
         {
             _messageRepository = messageRepository;
@@ -33,20 +34,27 @@ namespace API.SignalR
 
             var groupName = GetGroupName(Context.User.GetUsername(), otherUser);
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+            //3. add to the group
+            await AddToGroup(groupName);
 
             var messages = await _messageRepository.GetMessageThread(Context.User.GetUsername(), otherUser);
             await Clients.Group(groupName).SendAsync("ReceiveMessageThread", messages);
 
         }
-        
+
         public override async Task OnDisconnectedAsync(Exception exception)
         {
+            //4. remove from group
+            await RemoveFromMessageGroup();
+            //5. so what did all this work gave us? 
+            //  * we can use it in the send message method here:
+
             await base.OnDisconnectedAsync(exception);
         }
 
         public async Task SendMessage(CreateMessageDto createMessageDto)
         {
-            var username = Context.User.GetUsername(); 
+            var username = Context.User.GetUsername();
 
             if (username == createMessageDto.RecipientUsername.ToLower())
                 throw new HubException("You cannot send a messages to yourself!");
@@ -67,21 +75,58 @@ namespace API.SignalR
             };
 
             _messageRepository.AddMessage(message);
+            
+            //6. get the group name
+            var group = GetGroupName(sender.UserName, recipient.UserName);
+            var groupEntity = await _messageRepository.GetMessageGroup(group);      // get the group entity
+            if(groupEntity.Connections.Any(x => x.Username == recipient.UserName)){ // if the recipient is in the group (he is connected)
+                // we'll start using utc now (not local time) because of the way dotnet core works and calculate dates
+                // we'll use these in other couple of places 
+                // we'll talk about it later on, because we'll have a mix of different timezones
+                message.DateRead = DateTime.UtcNow;
+                // back to README.md
+            }
 
             if (await _messageRepository.SaveAllAsync())
             {
-                var group = GetGroupName(sender.UserName, recipient.UserName);
+                // we'll move this line up
+                // var group = GetGroupName(sender.UserName, recipient.UserName);
                 await Clients.Group(group).SendAsync("NewMessage", _mapper.Map<MessageDto>(message));
             }
 
         }
-        
+
+
         private string GetGroupName(string current, string other)
         {
             var stringCompare = string.CompareOrdinal(current, other) < 0;
             return stringCompare ? $"{current}-{other}" : $"{other}-{current}";
         }
 
+        //1. add a method to add to a group 
+        private async Task<bool> AddToGroup(string groupName)
+        {
+            var group = await _messageRepository.GetMessageGroup(groupName);
+            var connection = new Connection(Context.ConnectionId, Context.User.GetUsername());
 
+            // if the group is null, we need to create it
+            if (group == null)
+            {
+                group = new Group(groupName);
+                _messageRepository.AddGroup(group);
+            }
+
+            group.Connections.Add(connection);
+            return await _messageRepository.SaveAllAsync();
+        }
+
+        //2. add a method to remove from a message group
+        private async Task RemoveFromMessageGroup()
+        {
+            var connection = await _messageRepository.GetConnection(Context.ConnectionId);
+            _messageRepository.RemoveConnection(connection);
+            await _messageRepository.SaveAllAsync();
+            // now we have something to do when a user connect to disconnect from this message hub.
+        }
     }
 }
--- README.md
+++ README.md
@@ -1,26 +1,15 @@
-Tracking the message groups:
+Updating the message hub with group tracking:
 
-* this sounds like not so hard thing to do... but lets thing for a minute...
-* in MessageHub.cs we adding a user to a group, and on disconnection SignalR removing us from the group implicitly (we don;t need to do anything)
-* but  we have no way of knowing who is inside the group.
-* And the reason is (same as with the online presence) is that if we had more than one server, then SignalR has got no way of knowing if a user's connected to a different server.
-* So we have to do this ourselves, the original plan was to use the presence tracker inside PresenceTracker.cs
-* I though adding another dictionary to the OnlineUsers dictionary, but that would be a bit of a mess.
-* not just <username, connection list>, I though adding extra level, adding group name: <groupname, dict<username, connection list>>
-* anyway this became tricky and not practical.
+ok so now that we have our entities configured and our migration added, we can go back to our message hub.
+we'll start with adding some private methods, at the bottom of the file, handling adding and removing from a groups.
 
-* if you remember there ware 3 ways to do this that we talked about:
-1. in memory - we used it for online presence
-2. in a database(not the best idea) - user group are something that not really fit into the persistent data idea
-3. in a distributed cache (Redis) - optimal solution
+go to MessageHub.cs
 
-* lets look at option 2 now:
+ok so all that work just for a little functionality (a known misunderstanding between the 'dev' and the 'product' over time taking to implement a "small" feature)
 
-create new Entity called 'Group' and go to Group.cs
+test this out: 
+- two screens, sending chat messages, we see the 'unread' flag does not exist
+- moving one user away from the chat,other user send message, the 'unread' flag is set
+- moving the user back to the chat, the 'unread' flag is cleared from the other user chat screen.
 
-* ok so after all that we'll create a new migration to include our changes:
-* lets stop the API server and run `dotnet ef migrations add GroupsAdded`
-* lets see the migration file that was created.
-
-up next: see what we need to do in the message hub to make use of this.
-
+up next: dealing with the date issue and using utc time (it'll happens mostly when viewing in different browsers)
